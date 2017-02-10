80c5b4804a24af3a92f43cf78d8d31f600ef20c7
Nicholas Michel <nickmichel100@gmail.com>
2017-02-10 14:27:02 -0500

    removed sqlite gem

--- app/controllers/catalogues_controller.rb
+++ app/controllers/catalogues_controller.rb
@@ -3,8 +3,8 @@ class CataloguesController < ApplicationController
   end
 
   def pdf
-    pdf_filename = File.join(Rails.root, "app/assets/images/spirit sp 17catalog.pdf")
-    send_file(pdf_filename, :filename => "spirit sp 17catalog.pdf", :disposition => 'inline', :type => "application/pdf")
+    # pdf_filename = File.join(Rails.root, "app/assets/images/spirit sp 17catalog.pdf")
+    # send_file(pdf_filename, :filename => "spirit sp 17catalog.pdf", :disposition => 'inline', :type => "application/pdf")
   end
 
 end
