9a10a7ce4c503d6f857cf34e1a4d613e71fa2c06
Nicholas Michel <nickmichel100@gmail.com>
2017-08-01 17:29:42 -0400

    no bundle test 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,9 @@ set :pty, true
 # set :rvm1_map_bins, %w(rake gem bundle ruby honeybadger)
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
-# set :bundle_flags, '--deployment'
+set :bundle_flags, '--deployment'
+
+# set :bundle_path, -> { current_path }
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
