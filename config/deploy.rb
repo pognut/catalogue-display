c3810deddb7d6993a61396d9dedef6e44ee2a6bd
Nicholas Michel <nickmichel100@gmail.com>
2017-08-01 19:02:30 -0400

    changing unicorn gems back for a test, adding root role

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :pty, true
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 set :unicorn_config, "/etc/default/unicorn"
 set :unicorn_pid, "/var/run/unicorn.pid"
-
+set :unicorn_roles, "root"
 
 # set :bundle_flags, '--deployment'
 
