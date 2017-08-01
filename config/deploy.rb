c71aa774927c757a2e9e37067d0b3915e7d9aeb2
Nicholas Michel <nickmichel100@gmail.com>
2017-08-01 18:51:01 -0400

    changing unicorn gems 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,8 @@ set :pty, true
 
 # set :rvm1_map_bins, %w(rake gem bundle ruby honeybadger)
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
-# set :unicorn_config_path, "~/etc/default/unicorn"
-# set :unicorn_pid, "~/var/run/unicorn.pid"
+set :unicorn_config, "/etc/default/unicorn"
+set :unicorn_pid, "/var/run/unicorn.pid"
 
 
 # set :bundle_flags, '--deployment'
