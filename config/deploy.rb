bb3cc3181c544551b3748bb431ffa24f97a61457
Nicholas Michel <nickmichel100@gmail.com>
2017-08-01 18:02:59 -0400

    unicorn path fixes 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ set :pty, true
 # set :rvm1_map_bins, %w(rake gem bundle ruby honeybadger)
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
-set :unicorn_pid, "/var/run/unicorn.pid"
-set :unicorn_config_path, "/etc/default/unicorn"
+set :unicorn_pid, "~/../var/run/unicorn.pid"
+set :unicorn_config_path, "~/../etc/default/unicorn"
 
 # set :bundle_flags, '--deployment'
 
